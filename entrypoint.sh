2e97414949608c0bdc2c273933fa550b7ce57484
adriantandara <adriantandara2005@yahoo.com>
2024-12-12 04:26:36 +0200

    entrypoint.sh changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,16 +11,29 @@ while IFS= read -r ENV_VAR; do
     VAR_NAME=$(echo "$VAR_NAME" | sed 's/^OMP_//g' | sed 's/__/\./g' | tr '[:upper:]' '[:lower:]')
 
     if jq --exit-status ".${VAR_NAME}" config.json > /dev/null; then
-        jq ".${VAR_NAME} = \"${VAR_VALUE}\"" config.json > config.json.tmp
+        if [[ $VAR_VALUE =~ ^\[.*\]$ ]]; then
+            VAR_VALUE=$(echo "$VAR_VALUE" | sed 's/\[/"/g' | sed 's/\]/"/g' | sed 's/,/","/g')
+            jq ".${VAR_NAME} = [${VAR_VALUE}]" config.json > config.json.tmp
+        else
+            jq ".${VAR_NAME} = \"${VAR_VALUE}\"" config.json > config.json.tmp
+        fi
         mv config.json.tmp config.json
     else
-        jq ".${VAR_NAME} = \"${VAR_VALUE}\"" config.json > config.json.tmp
+        if [[ $VAR_VALUE =~ ^\[.*\]$ ]]; then
+            VAR_VALUE=$(echo "$VAR_VALUE" | sed 's/\[/"/g' | sed 's/\]/"/g' | sed 's/,/","/g')
+            jq ".${VAR_NAME} = [${VAR_VALUE}]" config.json > config.json.tmp
+        else
+            jq ".${VAR_NAME} = \"${VAR_VALUE}\"" config.json > config.json.tmp
+        fi
         mv config.json.tmp config.json
     fi
 
     OMP_CLI_ARGS+=("$VAR_NAME=$VAR_VALUE")
 
 done <<< "$ENV_OPENMP_VARS"
+
+echo "Config.json:"
+cat config.json
 
 if [ $# -gt 0 ]; then
     echo -e "\nAlternative launching method: $@"
