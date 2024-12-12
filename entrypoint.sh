f9595afe307a8a26f2880ec2d8aefbc619e5f7c3
adriantandara <adriantandara2005@yahoo.com>
2024-12-12 04:04:32 +0200

    entrypoint.sh changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,70 +1,35 @@
 #!/bin/bash
 cd /server
 
-#######
-#   APPLY ENVIRONMENT VARIABLES TO THE SERVER.CFG (LEGACY CONFIGURATION)
-#######
+update_config() {
+    local key=$1
+    local value=$2
+    local json_path=$3
 
-# If the configuration exists
-if [ -f server.cfg ]; then
-    # Get all environment variables starting with SAMP_
-    ENV_SAMP_VARS=$(env | grep '^SAMP_')
+    if [[ $value == \[* ]] || [[ $value == {* ]]; then
+        formatted_value=$value
+    else
+        formatted_value="\"$value\""
+    fi
 
-    # Create a temporary copy of server.cfg
-    cp server.cfg /tmp/server.cfg.tmp
+    jq "$json_path = $formatted_value" config.json > temp.json && mv temp.json config.json
+}
 
-    # Loop through each environment variable
-    while IFS= read -r ENV_VAR; do
-        # Temporarily change Internal Field Separator to '=' to split the variable into name and value
-        IFS='=' read -r VAR_NAME VAR_VALUE <<< "$ENV_VAR"
-
-        # Remove the SAMP_ prefix and convert the rest to lowercase
-        VAR_NAME=$(echo "$VAR_NAME" | sed 's/^SAMP_//g' | tr '[:upper:]' '[:lower:]')
-
-        # Check if the variable is already in the server configuration
-        if grep -q "^$VAR_NAME" /tmp/server.cfg.tmp; then
-            # If it is, replace the value
-            sed -i "s/^$VAR_NAME.*/$VAR_NAME $VAR_VALUE/g" /tmp/server.cfg.tmp
-        else
-            # If it isn't, add it to the end of the file
-            echo "$VAR_NAME $VAR_VALUE" >> /tmp/server.cfg.tmp
-        fi
-    done <<< "$ENV_SAMP_VARS"
-
-    # Write the content of the temporary file back to the server.cfg
-    cat /tmp/server.cfg.tmp > server.cfg
-    # Remove the temporary file
-    rm /tmp/server.cfg.tmp
-fi
-
-#######
-#   APPLY ENVIRONMENT VARIABLES TO THE open.mp LAUNCHING COMMAND
-#######
-
-# Initialize an empty array to store the environment variables
 OMP_CLI_ARGS=()
 
-# Get all environment variables starting with OMP_
 ENV_OPENMP_VARS=$(env | grep '^OMP_')
 
-# Loop through each environment variable
 while IFS= read -r ENV_VAR; do
-    # Temporarily change Internal Field Separator to '=' to split the variable into name and value
     IFS='=' read -r VAR_NAME VAR_VALUE <<< "$ENV_VAR"
 
-    # Remove the OMP_ prefix, convert double _ to . and convert the rest to lowercase
-    VAR_NAME=$(echo "$VAR_NAME" | sed 's/^OMP_//g' | sed 's/__/\./g' | tr '[:upper:]' '[:lower:]')
+    CLI_VAR_NAME=$(echo "$VAR_NAME" | sed 's/^OMP_//g' | sed 's/__/\./g' | tr '[:upper:]' '[:lower:]')
+    OMP_CLI_ARGS+=("$CLI_VAR_NAME=$VAR_VALUE")
 
-    # Add VAR_NAME=VAR_VALUE to the array
-    OMP_CLI_ARGS+=("$VAR_NAME=$VAR_VALUE")
+    JSON_VAR_NAME=$(echo "$VAR_NAME" | sed 's/^OMP_//' | tr '[:upper:]' '[:lower:]' | sed 's/__/./g')
+    update_config "$VAR_NAME" "$VAR_VALUE" ".$JSON_VAR_NAME"
 
 done <<< "$ENV_OPENMP_VARS"
 
-#######
-#   RUN THE SERVER
-#######
-
-# Either run the Dockerfile CMD or the open.mp server
 if [ $# -gt 0 ]; then
     echo -e "\nAlternative launching method: $@"
     sh -c "$@"
@@ -72,7 +37,6 @@ else
     ./omp-server -c "${OMP_CLI_ARGS[@]}"
 fi
 
-# Save the exit code of whatever we ran
 EXIT_CODE=$?
 
 exit $EXIT_CODE
